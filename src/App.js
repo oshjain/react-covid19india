1714338cb950a62efa88174ef531aaf8554593b3
moneybhadra <moneybhadra@gmail.com>
2020-04-08 09:51:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import * as Icon from 'react-feather';
+
+import './App.scss';
+
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Links from './components/links';
+import FAQ from './components/faq';
+import Banner from './components/banner';
+/* import PatientDB from './components/patientdb';*/
+
+const history = require('history').createBrowserHistory;
+
+function App() {
+  const pages = [
+    {
+      pageLink: '/',
+      view: Home,
+      displayName: 'Home',
+      animationDelayForNavbar: 0.2,
+    },
+    /* {
+      pageLink: '/database',
+      view: PatientDB,
+      displayName: 'Patients DB',
+      animationDelayForNavbar: 0.3,
+    },*/
+    {
+      pageLink: '/links',
+      view: Links,
+      displayName: 'Helpful Links',
+      animationDelayForNavbar: 0.5,
+    },
+    {
+      pageLink: '/faq',
+      view: FAQ,
+      displayName: 'FAQ',
+      animationDelayForNavbar: 0.6,
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Route
+          render={({location}) => (
+            <div className="Almighty-Router">
+              <Navbar pages={pages} />
+              <Banner />
+              <Route exact path="/" render={() => <Redirect to="/" />} />
+              <Switch location={location}>
+                {pages.map((page, i) => {
+                  return (
+                    <Route
+                      exact
+                      path={page.pageLink}
+                      component={page.view}
+                      key={i}
+                    />
+                  );
+                })}
+              </Switch>
+            </div>
+          )}
+        />
+      </Router>
+
+      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
+        {/* <img
+          src="/icon.png"
+          alt="https://www.covid19india.org | Coronavirus cases live dashboard"
+        />*/}
+
+        <h5>We stand with everyone fighting on the frontlines</h5>
+
+        <a
+          className="button excel"
+          href="https://bit.ly/patientdb"
+          target="_noblank"
+        >
+          <Icon.Database />
+          <span>Crowdsourced Patient Database&nbsp;</span>
+        </a>
+        <a
+          href="https://twitter.com/covid19indiaorg"
+          target="_noblank"
+          className="button twitter"
+          style={{justifyContent: 'center'}}
+        >
+          <Icon.Twitter />
+          <span>View updates on Twitter</span>
+        </a>
+        <h3>Stay indoors, stay safe</h3>
+        <h1>
+          <Icon.Heart color="red" />
+        </h1>
+        <h1>HJ</h1>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
